f923e56a871daa7732e43382e08792b4ae6559e6
monis07 <monisazeem@gmail.com>
2024-05-02 15:10:32 +0530

    updated dist

--- server/dist/index.js
+++ server/dist/index.js
@@ -12,10 +12,7 @@ const port = 3000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(body_parser_1.default.json());
-app.use((0, cors_1.default)({
-    origin: 'https://logicloom-client.vercel.app',
-    credentials: true // If you're using credentials like cookies or Authorization header
-}));
+app.use((0, cors_1.default)());
 app.use('/admin', admin_1.default);
 app.listen(port, () => {
     console.log("Server is listening on port " + port);
